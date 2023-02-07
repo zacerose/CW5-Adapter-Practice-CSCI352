ea05a4ef5b4b755dabb3f48a33f060fd972783bb
Zachary Rose <zacerose@ut.utm.edu>
2023-02-07 13:35:30 -0600

    Added interfaces, classes, and some commenting.

--- CW5-Adapter_Practice/Program.cs
+++ CW5-Adapter_Practice/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// Name: Zachary Rose
+// Date: 2/7/2023
+// CSCI352
+// A basic example of an adapter.The client requests a toybear, but by
+//   implementing the bear adapter, we can disguise a grizzle bear
+//   as a toybear.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +12,67 @@ using System.Threading.Tasks;
 
 namespace CW5_Adapter_Practice
 {
+    interface Bear
+    {
+        void maul();
+        void hibernate();
+    }
+    class Grizzly : Bear
+    {
+        public void maul()
+        {
+            Console.WriteLine("Bear uses: maul!");
+        }
+        public void hibernate()
+        {
+            Console.WriteLine("Bear uses: hibernate!");
+        }
+    }
+    interface ToyBear
+    {
+        void hug();
+    }
+    class TeddyBear : ToyBear
+    {
+        public void hug()
+        {
+            Console.WriteLine("Bear uses: hug!");
+        }
+    }
+    class BearAdapter : ToyBear 
+    {
+        Bear bear;
+        public BearAdapter(Bear bear)
+        {
+            this.bear = bear;
+        }
+        public void hug()
+        {
+            bear.maul();
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Bear grizzly = new Grizzly();
+            ToyBear teddybear = new TeddyBear();
+            ToyBear secretGrizzly = new BearAdapter(grizzly);
+
+            // Bear functions
+            Console.WriteLine("Bear functions print test:");
+
+            grizzly.maul();
+            grizzly.hibernate();
+
+            // Toybear functions
+            Console.WriteLine("Toybear functions print test:");
+
+            teddybear.hug();
+
+            secretGrizzly.hug();
+
+            Console.ReadKey();
         }
     }
 }
